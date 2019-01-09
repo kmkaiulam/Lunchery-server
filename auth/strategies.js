4fea6afc0a5e2cb995e0cea4cbeeb8cd397479df
Kenny Lam <kmlam@ucdavis.edu>
2019-01-09 14:26:54 -0800

    jwt strategy modify

--- auth/strategies.js
+++ auth/strategies.js
@@ -1,7 +1,8 @@
 'use strict';
 const passport = require('passport')
-const {Strategy: LocalStrategy} = require('passport-local');
-const {Strategy: JwtStrategy, ExtractJwt} = require('passport-jwt');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const {User} = require('../models')
 const {JWT_SECRET} = require('../config');
 
