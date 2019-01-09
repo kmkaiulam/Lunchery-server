19ab4ecca87aaa4288436420325b96313d11c1b2
Kenny Lam <kmlam@ucdavis.edu>
2019-01-09 12:22:30 -0800

    remove unused param

--- config.js
+++ config.js
@@ -6,6 +6,5 @@ exports.JWT_SECRET=process.env.JWT_SECRET;
 exports.JWT_EXPIRY=process.env.JWT_EXPIRY;
 exports.S3_BUCKET_NAME=process.env.S3_BUCKET_NAME;
 exports.AWS_ACCESS_KEY=process.env.AWS_ACCESS_KEY;
-exports.AWS_REGION=process.env.AWS_REGION;
 exports.AWS_Uploaded_File_URL_LINK=process.env.AWS_Uploaded_File_URL_LINK;
 exports.AWS_USER_ARN=process.env.AWS_USER_ARN;
