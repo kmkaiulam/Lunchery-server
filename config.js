1d6f01ce97ee46d541f6195c063d5ac1cc4d1373
Kenny Lam <kmlam@ucdavis.edu>
2019-01-09 11:58:23 -0800

    clean up config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL  || 'mongodb://localhost/lunchery'
-exports.TEST_DATABASE_URL =  process.env.TEST_DATABASE_URL || 'mongodb://localhost/lunchery-test'  
-exports.PORT = process.env.PORT;
-exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
+exports.DATABASE_URL=process.env.DATABASE_URL  || 'mongodb://localhost/lunchery'
+exports.TEST_DATABASE_URL=process.env.TEST_DATABASE_URL || 'mongodb://localhost/lunchery-test'  
+exports.PORT=process.env.PORT;
+exports.JWT_SECRET=process.env.JWT_SECRET;
+exports.JWT_EXPIRY=process.env.JWT_EXPIRY;
 exports.S3_BUCKET_NAME=process.env.S3_BUCKET_NAME;
 exports.AWS_ACCESS_KEY=process.env.AWS_ACCESS_KEY;
 exports.AWS_REGION=process.env.AWS_REGION;
